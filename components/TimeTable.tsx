686ba04a1a94dbd2f434b00accf6c3339aaf1d9d
Denny Huang <denny0223@gmail.com>
2024-12-06 23:36:34 +0800

    fest: time table add record

--- components/TimeTable.tsx
+++ components/TimeTable.tsx
@@ -91,6 +91,7 @@ export default function TimeTable() {
     speakers: any;
     slide: string;
     co_write: string;
+    record: string;
   }
 
   const [sessionMessage, setSessionMessage] = useState<SessionMessage | null>(
@@ -363,6 +364,15 @@ export default function TimeTable() {
                         className="slide"
                       >
                         課程共筆
+                      </a>
+                    )}
+                    {sessionMessage.record && (
+                      <a
+                        href={sessionMessage.record}
+                        target="_blank"
+                        className="slide"
+                      >
+                        課程錄影
                       </a>
                     )}
                   </div>
